764b49634f9edbfc2def3f412c28db1118e4d951
programster <sdpagent@gmail.com>
2014-09-11 19:22:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ import sys
 import subprocess
 
 num_cpu = multiprocessing.cpu_count()
-
 job_pool = multiprocessing.Pool(num_cpu)
 
 # Fetch the commands file path which contains each command we wish to run on a separate line
@@ -18,8 +17,6 @@ cmds_fp = sys.argv[1]
 lines = [line.strip() for line in open(cmds_fp, 'r')]
 
 def process_line(line_command):
-    # http://stackoverflow.com/questions/89228/calling-an-external-command-in-python
-    print "running " + line_command
     subprocess.check_call(line_command, shell=True)
 
 job_pool.map(process_line, lines)
